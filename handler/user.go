521472ca148513c206485bc34d0c4905e36586e5
Fatah <fatahhikami@gmail.com>
2021-04-05 17:12:06 +0700

    Define handler & main.go

--- handler/user.go
+++ handler/user.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"bwaproject/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (handler *userHandler) RegisterUser(context *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := context.ShouldBindJSON(&input)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, nil)
+	}
+
+	user, err := handler.userService.RegisterUser(input)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, nil)
+	}
+
+	context.JSON(http.StatusOK, user)
+}
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bwaproject/handler"
+	"bwaproject/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "newuser:newuser@tcp(127.0.0.1:3306)/golangbwa?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+
+	api := router.Group("/api/v1")
+	{
+		api.POST("/users", userHandler.RegisterUser)
+	}
+
+	router.Run()
+}
